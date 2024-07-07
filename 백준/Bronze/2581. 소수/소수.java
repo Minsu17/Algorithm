d4a221d62985178f352747aefaf722dc22c27d8e
Minsu17 <89891511+Minsu17@users.noreply.github.com>
2024-07-07 19:22:02 +0900

    [Bronze II] Title: 소수, Time: 184 ms, Memory: 16256 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int M = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		int min = N;
+		int sum = 0;
+		
+		for(int i = M; i <= N; i++) {
+			//소수인지 판별 변수
+			boolean check = true;
+			//1이 올 수 있지만 1은 소수가 아니기에 chech에 false 저장
+			if(i == 1) check = false;
+			
+			for(int j = 2; j < i; j++) {
+				//나누어 떨어진다면 check에 false 저장
+				if(i % j == 0) {
+					check = false;
+					break;
+				}
+			}
+			
+			//check가 true면 실수
+			if(check) {
+				if(min > i) min = i;
+				sum += i;
+			}
+		}
+		if(sum == 0) {
+			System.out.println(-1);
+		}else {
+			System.out.println(sum + "\n" + min);
+		}
+	}
+
+}
