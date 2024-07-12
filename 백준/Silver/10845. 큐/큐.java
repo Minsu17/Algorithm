69d45e21f456cfaf3679670faf6fefdea424c712
Minsu17 <89891511+Minsu17@users.noreply.github.com>
2024-07-12 22:21:48 +0900

    [Silver IV] Title: 큐, Time: 320 ms, Memory: 25148 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue<Integer> q = new LinkedList<>();
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < N; i++) {
+			String[] s = br.readLine().split(" ");
+			switch(s[0]) {
+			case "push":
+				q.offer(Integer.parseInt(s[1]));
+				break;
+				
+			case "pop":
+				if(q.isEmpty()) System.out.println(-1);
+				else System.out.println(q.poll());
+				break;
+				
+			case "size":
+				System.out.println(q.size());
+				break;
+				
+			case "empty":
+				if(q.isEmpty()) System.out.println(1);
+				else System.out.println(0);
+				break;
+				
+			case "front":
+				if(q.isEmpty()) System.out.println(-1);
+				else System.out.println(q.peek());
+				break;
+				
+			case "back":
+				if(!q.isEmpty()) {
+					int last = 0;
+					ArrayList<Integer> list = new ArrayList<>();
+					while(!q.isEmpty()) {
+						
+						last = Integer.valueOf(q.poll());
+						list.add(last);
+					}
+					System.out.println(last);
+					for(int a : list) {
+						q.offer(a);
+					}
+				}
+				else {
+					System.out.println(-1);
+				}
+			}
+		}
+    }
+}
