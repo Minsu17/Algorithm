45822d03d977bc356a5f60917e8239cdf8594326
Minsu17 <89891511+Minsu17@users.noreply.github.com>
2024-07-12 22:11:39 +0900

    [Silver IV] Title: 스택, Time: 272 ms, Memory: 20820 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> st = new Stack<>();
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < N; i++) {
+			String[] s = br.readLine().split(" ");
+			switch(s[0]) {
+			case "push":
+				st.push(Integer.parseInt(s[1]));
+				break;
+			case "pop":
+				if(st.isEmpty()) System.out.println(-1);
+				else System.out.println(st.pop());
+				break;
+			case "size":
+				System.out.println(st.size());
+				break;
+			case "empty":
+				if(st.isEmpty()) System.out.println(1);
+				else System.out.println(0);
+				break;
+			case "top":
+				if(st.isEmpty()) System.out.println(-1);
+				else System.out.println(st.peek());
+				break;
+			}
+		}
+    }
+}
