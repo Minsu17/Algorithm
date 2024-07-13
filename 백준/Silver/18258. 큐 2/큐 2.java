7d98e7bce69530bc0cff6e07f74c1fb34ab002ec
Minsu17 <89891511+Minsu17@users.noreply.github.com>
2024-07-13 15:59:35 +0900

    [Silver IV] Title: 큐 2, Time: 1400 ms, Memory: 409520 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue<Integer> q = new LinkedList<>();
+        
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int last = 0;
+        
+        for (int i = 0; i < N; i++) {
+			String[] s = br.readLine().split(" ");
+			switch(s[0]) {
+			case "push":
+				q.offer(Integer.parseInt(s[1]));
+				last = Integer.parseInt(s[1]);
+				break;
+				
+			case "pop":
+				if(q.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(q.poll()).append("\n");
+				break;
+				
+			case "size":
+				sb.append(q.size()).append("\n");
+				break;
+				
+			case "empty":
+				if(q.isEmpty()) sb.append(1).append("\n");
+				else sb.append(0).append("\n");
+				break;
+				
+			case "front":
+				if(q.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(q.peek()).append("\n");
+				break;
+				
+			case "back":
+				if(!q.isEmpty()) {
+					sb.append(last).append("\n");
+				}
+				else {
+					sb.append(-1).append("\n");
+				}
+			}
+			
+		}
+        System.out.println(sb);
+    }
+}
