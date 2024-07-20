727566b2af9bb59b762bc37d22600eed2a0c88f9
Minsu17 <89891511+Minsu17@users.noreply.github.com>
2024-07-20 20:56:41 +0900

    [Silver V] Title: 다리 놓기, Time: 156 ms, Memory: 14468 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+	static int[][] check = new int[30][30];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			System.out.println(dp(M, N)); //M개에서 N개를 뽑는 조합공식
+		}
+    }
+    
+    public static int dp(int m, int n) {
+    	if(check[m][n] != 0) return check[m][n];
+    		
+    	if(m == n || n == 0) return check[m][m] = 1; //조합의 성질
+    	
+    	return check[m][n] = dp(m-1, n-1) + dp(m-1, n);
+    }
+}
