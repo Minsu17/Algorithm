5aef2bfeb66c7fde51f9359e81446c149a3f8fec
Minsu17 <89891511+Minsu17@users.noreply.github.com>
2024-07-22 21:07:56 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1496 ms, Memory: 107476 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M, R, cnt;
+    static int[] visited;
+    static List<Integer>[] adjList;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        adjList = new LinkedList[N + 1];
+        for (int i = 0; i <= N; i++) {
+            adjList[i] = new LinkedList<Integer>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            adjList[u].add(v);
+            adjList[v].add(u);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(adjList[i]);
+        }
+
+        visited = new int[N + 1];
+        cnt = 1;
+
+        bfs(R);
+
+        for (int i = 1; i <= N; i++) {
+            System.out.println(visited[i]);
+        }
+    }
+
+    static void bfs(int n) {
+        Queue<Integer> q = new ArrayDeque<>();
+
+        visited[n] = cnt++;
+        q.add(n);
+
+        while (!q.isEmpty()) {
+            int tmp = q.poll();
+
+            for (int next : adjList[tmp]) {
+                if (visited[next] == 0) {
+                    visited[next] = cnt++;
+                    q.add(next);
+                }
+            }
+        }
+    }
+}
