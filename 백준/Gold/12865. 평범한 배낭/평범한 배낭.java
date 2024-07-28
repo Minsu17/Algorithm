236050aa9d577831edcee70b5eff3e02f0a393ae
Minsu17 <89891511+Minsu17@users.noreply.github.com>
2024-07-28 23:22:00 +0900

    [Gold V] Title: 평범한 배낭, Time: 196 ms, Memory: 53836 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		int[] weights = new int[N+1];
+		int[] values = new int[N+1];
+		
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			weights[i] = Integer.parseInt(st.nextToken());
+			values[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[][] dp = new int[N+1][K+1];
+		
+		for (int i = 1; i <= N; i++) {
+			for (int w = 0; w <= K; w++) {
+				if (weights[i] <= w) {
+					// 현재 물품을 넣을 수 있는 경우
+                    // 물품을 넣는 경우와 넣지 않는 경우 중 최대 가치를 선택
+					dp[i][w] = Math.max(dp[i-1][w], dp[i-1][w-weights[i]] + values[i]);
+				} else {
+					 // 현재 물품을 넣을 수 없는 경우
+					dp[i][w] = dp[i-1][w];
+				}
+			}
+		}
+		
+		System.out.println(dp[N][K]);
+	}
+}
