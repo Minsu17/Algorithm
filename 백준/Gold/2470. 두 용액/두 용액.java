0321cf630551b6b6ca30c91310a373773d0c10b3
Minsu17 <89891511+Minsu17@users.noreply.github.com>
2024-08-07 21:20:42 +0900

    [Gold V] Title: 두 용액, Time: 384 ms, Memory: 29456 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		int start = 0;
+		int end = N-1;
+		int closeSum = Integer.MAX_VALUE;
+		int left = start;
+		int right = end;
+		
+		while(start < end) {
+			int sum = arr[start] + arr[end];
+			
+			if(Math.abs(sum) < Math.abs(closeSum)) {
+				closeSum = sum;
+				//합이 0에 가장 가까웠던 왼쪽인덱스와 오른쪽인덱스 left, right에 값 최신화
+				left = start;
+				right = end;
+			}
+			
+			if(sum < 0) start++;
+			else if(sum > 0) end--;
+			else break;
+		}
+		
+		System.out.print(arr[left] + " " + arr[right]);
+    }
+}
