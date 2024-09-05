3f6917ea499805e36111157fe2a74d2fc58b3907
Minsu17 <89891511+Minsu17@users.noreply.github.com>
2024-09-05 19:45:00 +0900

    [level 2] Title: 카펫, Time: 27.36 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        int sum = brown + yellow;
+        
+        for(int i=1;i<=brown;i++){
+            for(int j=1;j<=brown;j++){
+                if((i-2)*(j-2) == yellow && i * j == sum){
+                    answer[0] = i;
+                    answer[1] = j;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
