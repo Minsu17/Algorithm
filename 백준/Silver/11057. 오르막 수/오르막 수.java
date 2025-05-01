b6b6d545b01a82ef1391206c0dae59bc8a0ce597
Minsu17 <89891511+Minsu17@users.noreply.github.com>
2025-05-01 22:35:15 +0900

    [Silver I] Title: 오르막 수, Time: 104 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    static final int MOD = 10007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N + 1][10];
+
+        for (int j = 0; j < 10; j++) {
+            dp[1][j] = 1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = j; k < 10; k++) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][k]) % MOD;
+                }
+            }
+        }
+
+        int result = 0;
+        for (int j = 0; j < 10; j++) {
+            result = (result + dp[N][j]) % MOD;
+        }
+
+        System.out.println(result);
+    }
+}
