f6fb08828a26a309f8b9816c372ba509c7d69ddc
Minsu17 <89891511+Minsu17@users.noreply.github.com>
2025-09-18 17:05:49 +0900

    [level 1] Title: 폰켓몬, Time: 4.97 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,17 +2,17 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        HashSet<Integer> h = new HashSet<>();
         
-        for(int i=0;i<nums.length;i++){
-            h.add(nums[i]);
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int num : nums){
+            set.add(num);
         }
-        
-        if(h.size() > nums.length/2){
+        if(set.size() > nums.length/2){
             answer = nums.length/2;
         }
         else{
-            answer = h.size();
+            answer = set.size();
         }
         return answer;
     }
