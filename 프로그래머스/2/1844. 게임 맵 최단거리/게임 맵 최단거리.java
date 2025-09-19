b18ac2c75550e44b6f4e17cb37244cae5f7d7a7d
Minsu17 <89891511+Minsu17@users.noreply.github.com>
2025-09-19 15:32:34 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.10 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -12,14 +12,13 @@ class Solution {
         
         q.offer(new int[]{0, 0, 1});
         visited[0][0] = true;
-        
         while(!q.isEmpty()){
             int[] cur = q.poll();
             int x = cur[0];
             int y = cur[1];
             int dist = cur[2];
             
-            if (x == n - 1 && y == m - 1) {
+            if(x == n -1 && y == m - 1){
                 return dist;
             }
             
