dcf05b92f5b6d4bb46ee9a345e0b92f2e24b15c6
Minsu17 <89891511+Minsu17@users.noreply.github.com>
2025-09-26 15:11:52 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.86 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,27 +1,21 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] numbers) {
-        ArrayList<Integer> list = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
         
-        for(int i=0;i<numbers.length - 1;i++){
-            int sum = 0;
+        for(int i=0;i<numbers.length-1;i++){
             for(int j=i+1;j<numbers.length;j++){
-                sum = numbers[i] + numbers[j];
-                list.add(sum);
+                int sum = numbers[i] + numbers[j];
+                set.add(sum);
             }
         }
-        for (int i = list.size() - 1; i >= 0; i--) {
-             for (int j = i - 1; j >= 0; j--) {
-                if (list.get(i).equals(list.get(j))) {
-                    list.remove(i);
-                    break;
-                }
-            }
-}
-        int[] answer = new int[list.size()];
-        for(int i=0;i<list.size();i++){
-            answer[i] = list.get(i);
+        int size = set.size();
+        int[] answer = new int[size];
+        int k = 0;
+        
+        for(int num : set){
+            answer[k] = num;
+            k++;
         }
         Arrays.sort(answer);
         return answer;
