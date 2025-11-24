9ee9052077d7314bccefb2e015fd5affcbcc3260
Minsu17 <89891511+Minsu17@users.noreply.github.com>
2025-11-24 22:47:33 +0900

    [level 3] Title: 네트워크, Time: 0.19 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,14 +1,11 @@
-import java.util.*;
 class Solution {
     static boolean[] visited;
     public int solution(int n, int[][] computers) {
         int answer = 0;
         visited = new boolean[computers.length];
         
-        Arrays.fill(visited, false);
-        
         for(int i=0;i<computers.length;i++){
-            if(visited[i] == false){
+            if(!visited[i]){
                 answer++;
                 dfs(i, computers);
             }
